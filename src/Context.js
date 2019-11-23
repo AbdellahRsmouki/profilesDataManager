e1b1747d079027816f04115b0b1bda1f138f3029
AbdellahRs <abdullah.rsmouki@gmail.com>
2019-11-23 04:01:18 +0000

    all search params are working
    TODO: costumising

--- src/Context.js
+++ src/Context.js
@@ -57,7 +57,7 @@ class ProfilesProvider extends Component {
 
     handleChange = event =>{
         const target = event.target;
-        const value = event.type === 'checkbox' ?
+        const value = target.type === 'checkbox' ?
         target.checked:target.value;
         const name = event.target.name;
         this.setState({
@@ -67,10 +67,38 @@ class ProfilesProvider extends Component {
 
     filterProfiles = () => {
         let {profiles,type, capacity, price, minSize, maxSize,breakfast, pets} = this.state;
+        // all the profiles
         let tempProfiles = [...profiles];
+        // transform values
+        capacity = parseInt(capacity);
+        price = parseInt(price);
+        // filter by type
         if(type!=='all'){
             tempProfiles = tempProfiles.filter(profile=>profile.type===type)
         }
+
+        // filter by price
+        tempProfiles = tempProfiles.filter(profile=>profile.price<=price)
+
+        // filter by capacity
+        if(capacity!==1){
+            tempProfiles = tempProfiles.filter(profile=>profile.capacity>=capacity)
+        }
+
+        // filter by size
+        tempProfiles = tempProfiles.filter(profile=>profile.size<=maxSize && profile.size>=minSize)
+
+        // filter by pets
+        if(pets){
+            tempProfiles = tempProfiles.filter(profile=>profile.pets===true)
+        }
+
+        // filter by breakfast
+        if(breakfast){
+            tempProfiles = tempProfiles.filter(profile=>profile.breakfast===true)
+        }
+
+        //change state
         this.setState({
             sortedProfiles:tempProfiles
         });
--- src/components/ProfilesFilter.js
+++ src/components/ProfilesFilter.js
@@ -22,7 +22,11 @@ types= ['all',...types];
 // map to jsx
 types = types.map((item,index) => {
     return <option value={item} key={index}>{item}</option>
-})
+});
+let people = getUnique(profiles,'capacity');
+people = people .map((item,index) => {
+    return <option value={item} key={index}>{item}</option>
+});
     return (
         <section className="filter-container">
             <Title title="Search Profiles" />
@@ -36,6 +40,45 @@ types = types.map((item,index) => {
                     </select>
                 </div>
                 {/*end of select type*/}
+                {/*guests*/}
+                <div className="form-group">
+                    <label htmlFor="capacity">Guest</label>
+                    <select name="capacity" id="capacity" value={capacity}
+                    className="form-control" onChange={handleChange}>
+                        {people}
+                    </select>
+                </div>
+                {/*end of guests*/}
+                {/*room price*/}
+                <div className="form-group">
+                    <label htmlFor="price">Room price ${price}</label>
+                    <input name="price" value={price} id="price" type="range" min={minPrice} max={maxPrice}
+                    className="form-control" onChange={handleChange} />
+                </div>
+                {/*end of room price*/}
+                {/*room size*/}
+                <div className="form-group">
+                    <label htmlFor="size">Room size</label>
+                    <div className="size-inputs">
+                    <input name="minSize" value={minSize} id="size" type="number" 
+                    className="size-input" onChange={handleChange} />
+                    <input name="maxSize" value={maxSize} id="size" type="number" 
+                    className="size-input" onChange={handleChange} />
+                    </div>
+                </div>
+                {/*end of room size*/}
+                {/*extras*/}
+                <div className="form-group">
+                    <div className="single-extra">
+                        <input name="breakfast" id="breakfast" type="checkbox" checked={breackfast} onChange={handleChange}/>
+                        <label htmlFor="breakfast">breackfast</label>
+                    </div>
+                    <div className="single-extra">
+                    <input name="pets" id="pets" type="checkbox" checked={pets} onChange={handleChange}/>
+                    <label htmlFor="pets">pets</label>
+                    </div>
+                </div>
+                {/*end of extras*/}
             </form>
         </section>
     )
